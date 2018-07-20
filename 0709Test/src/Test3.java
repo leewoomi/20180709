4f3d7f4202f78cb20b58e5c8e7f742b94e603d41
leewoomi <woomi1114@gmail.com>
2018-07-20 09:33:12 +0900

    Test3

--- 0709Test/src/Test3.java
+++ 0709Test/src/Test3.java
@@ -11,6 +11,28 @@ public class Test3 {
 				  90123456789
 					 */
 			
+		int idx =0;
+		for(int i=0; i<6; i++) {
+			int j;
+			for(j=0; j<=5-i;j++) {
+				System.out.print(" ");
+			}System.out.print(idx % 10);
+			idx +=1;
+			if (i>=1 && i <=4) {
+				for(j=0;j<2*i-1;j++) {
+					System.out.print(" ");
+				}
+				System.out.print(idx % 10);
+				idx += 1;
+			}
+			if(i==5) {
+				for(j=0;j<2*i;j++) {
+					System.out.print(idx % 10);
+					idx +=1;
+				}
+			}
+			System.out.println(" ");
+		}
 			
 	}
 
